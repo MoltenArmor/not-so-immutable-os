76d7c5a634c3afc53406ac6c11fcfbce7bec9eb5
MoltenArmor <kwokjuy@163.com>
2025-03-02 19:23:17 +0800

    [FIX] Fix scripts

--- scripts/update-all-confexts.sh
+++ scripts/update-all-confexts.sh
@@ -5,7 +5,7 @@ SCRIPT_PATH="$(readlink -nf "$0")"
 SCRIPT_DIR="$(dirname "${SCRIPT_PATH}")"
 OUTPUT_PATH="${SCRIPT_DIR}/../mkosi.output"
 
-for confext_image_path in "${OUTPUT_PATH}"/confext-*; do
+for confext_image_path in "${OUTPUT_PATH}"/confext-*.raw; do
     confext_image_basename="$(basename "${confext_image_path}")"
     if [ -f "/var/lib/confexts/${confext_image_basename}" ]; then
         rm -f "/var/lib/confexts/${confext_image_basename}"
--- scripts/update-all-sysexts.sh
+++ scripts/update-all-sysexts.sh
@@ -5,7 +5,7 @@ SCRIPT_PATH="$(readlink -nf "$0")"
 SCRIPT_DIR="$(dirname "${SCRIPT_PATH}")"
 OUTPUT_PATH="${SCRIPT_DIR}/../mkosi.output"
 
-for sysext_image_path in "${OUTPUT_PATH}"/sysext-*; do
+for sysext_image_path in "${OUTPUT_PATH}"/sysext-*.raw; do
     sysext_image_basename="$(basename "${sysext_image_path}")"
     if [ -f "/var/lib/extensions/${sysext_image_basename}" ]; then
         rm -f "/var/lib/extensions/${sysext_image_basename}"
