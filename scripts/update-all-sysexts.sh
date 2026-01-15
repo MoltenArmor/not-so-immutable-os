0f724591f9da7acdc4de7820dd05b2b190617cc4
MoltenArmor <kwokjuy@163.com>
2026-01-15 23:22:27 +0800

    Make extension multi-modal

--- scripts/update-all-sysexts.sh
+++ scripts/update-all-sysexts.sh
@@ -5,12 +5,14 @@ SCRIPT_PATH="$(readlink -nf "$0")"
 SCRIPT_DIR="$(dirname "${SCRIPT_PATH}")"
 OUTPUT_PATH="${SCRIPT_DIR}/../mkosi.output"
 
-for sysext_image_path in "${OUTPUT_PATH}"/sysext-*.raw; do
-    sysext_image_basename="$(basename "${sysext_image_path}")"
-    if [ -f "/var/lib/extensions/${sysext_image_basename}" ]; then
-        rm -f "/var/lib/extensions/${sysext_image_basename}"
+for ext_image_path in "${OUTPUT_PATH}"/ext-*.raw; do
+    ext_image_basename="$(basename "${ext_image_path}")"
+    if [ -f "/var/lib/extensions/${ext_image_basename}" ]; then
+        rm -f "/var/lib/extensions/${ext_image_basename}"
     fi
-    importctl import-raw "${sysext_image_path}" --class=sysext
+    importctl import-raw "${ext_image_path}" --class=sysext
+
+    ln -s "../extensions/${ext_image_basename}" "/var/lib/confexts/${ext_image_basename}"
 done
 
-printf '%s\n' 'Import done! Please execute "systemctl reload systemd-sysext.service"!'
+printf '%s\n' "Import done! Please execute 'systemctl reload systemd-sysext.service systemd-confext.service'!"
